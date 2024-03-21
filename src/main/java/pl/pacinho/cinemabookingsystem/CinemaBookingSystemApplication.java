f603fce2572961bc67dbc52eab8316f6ea30c8b8
patryk_ojdana <patryk.ojdana@axfina.pl>
2024-03-21 09:45:34 +0100

    Init

---
+++ src/main/java/pl/pacinho/cinemabookingsystem/CinemaBookingSystemApplication.java
@@ -0,0 +1,13 @@
+package pl.pacinho.cinemabookingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaBookingSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/pl/pacinho/cinemabookingsystem/config/Properties.java
@@ -0,0 +1,8 @@
+package pl.pacinho.cinemabookingsystem.config;
+
+public class Properties {
+
+    public static final String NAME = "Cinema Booking System";
+    public static final String VERSION = "0.0.1-BETA";
+    public static final String DESCRIPTION = "API for cinema booking system";
+}
